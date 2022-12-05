197911dedcb43c690fe44bea6ae29545a0b8c50b
ITS-VING <119536363+ITS-VING@users.noreply.github.com>
2022-12-05 20:47:40 +0530

    Update play.py

--- Zaid/plugins/play.py
+++ Zaid/plugins/play.py
@@ -550,3 +550,46 @@ async def kickedvc(_, chat_id: int):
         clear_queue(chat_id)
     if chat_id in active:
         active.remove(chat_id)
+
+@Zaid.on(events.NewMessage(pattern="^/broadcast"))
+@sudo_users_only
+async def broadcast_message_nopin(c: Client, message: Message):
+    if not message.reply_to_message:
+        pass
+    else:
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await c.forward_messages(i, y, x)
+                await asyncio.sleep(0.3)
+                sent += 1
+            except Exception:
+                pass
+        await message.reply_text(f"✅ Broadcast complete in {sent} Group.")
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**usage**:\n\n/broadcast (`message`) or (`reply to message`)"
+        )
+        return
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    chats = []
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await c.send_message(i, text=text)
+            await asyncio.sleep(0.3)
+            sent += 1
+        except Exception:
+            pass
+    await message.reply_text(f"✅ Broadcast complete in {sent} Group.")
+
